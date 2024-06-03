c6ac86a7125802b8b516b9127c92d3b79d40d04a
Daniel Jaimes <daniel.jaimes@soprasteria.com>
2024-06-03 13:46:49 +0200

    Feuture component heroes-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        HeroesComponent
+        HeroesComponent,
+        
     ],
     declarations: [
         AppComponent,
